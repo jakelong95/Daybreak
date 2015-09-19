6959665116b4c581628f00ade5c4979e7df8332c
Flavia Roma Cavalcanti <flaviarc@iastate.edu>
2015-09-18 23:25:12 -0300

    initial commit

---
+++ DayBreak_ArcadeMode/src/Room.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author flavia
+ * Our room object for the arcade mode 
+ */
+public class Room {
+	private int width; //width of the room
+	private int height; //height of the room
+	private int numOpenings; //number of openings in a given room
+	
+	
+
+}
