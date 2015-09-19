69690e4e00316995d31d49d8e9bac9964d98339d
Jake Long <jakeprogrammer@gmail.com>
2015-09-19 00:24:45 -0500

    Changed TypeType enum to constants so they can be used as array indices. Changed variable name capitalization to conform with standards.

--- src/daybreak/Tile.java
+++ src/daybreak/Tile.java
@@ -3,22 +3,19 @@ package daybreak;
 import org.newdawn.slick.Image;
 
 
-public class Tile {
+public class Tile
+{
+	//Tile types
+	public static final int FLOOR = 0; //Walkable background
+	public static final int WALL = 1;  //Wall that cannot be passed through
+	public static final int DOOR = 2;  //Closed door that AI cannot pass through
 
-	public enum TileType
-	{
-		Cobble, //Walkable background
-		Wall, //Cannot be destroyed. Always exterior.
-		CrackedWall //Cracked wall can be destroyed with rocket launcher. 
-	}
-
-	public TileType TerrainType; //The terrain type
-	public Image TileGFX; 
+	public int terrainType; //The terrain type
+	public Image tileImage; 
 	//public Rectangle SrcRect; //The source from which we get the tile (may be unecessary?)
 	//public Vector2d Location //Where we will draw it on screen.
 	public boolean isBlocked; //If true, we cannot walk or shoot through this.
 	
 	static Image[] tileGFX; //Array of tiles to populate base.
-	
 }
 
