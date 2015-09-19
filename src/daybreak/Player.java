852a434b5b9a316240f7c298ecc4191ed5e0562f
Jake Long <jakeprogrammer@gmail.com>
2015-09-19 11:21:55 -0500

    Player now handles user input for movement.

--- src/daybreak/Player.java
+++ src/daybreak/Player.java
@@ -1,5 +1,9 @@
 package daybreak;
 
+import org.newdawn.slick.Input;
+
+import static org.newdawn.slick.Input.*;
+
 /**
  * Represents the player.
  * 
@@ -7,10 +11,57 @@ package daybreak;
  */
 public class Player extends Entity
 {
+	//Reference to the input manager
+	private Input input;
+	
+	/**
+	 * Creates a new player.
+	 * @param map Reference to the game map.
+	 */
+	public Player(Tile[][] map)
+	{
+		super(map);
+		
+		this.input = input;
+	}
+
+	//How many vamps the player has killed
+	private int killCount;
+	
 	//TODO - Add weapons, etc.
+	
 	@Override
 	public void update(int deltaTime)
 	{
-		//TODO - Handle user input
+		//Check if the player is pressing any of the movement keys
+		if(input.isKeyDown(KEY_W) || input.isKeyDown(KEY_UP))
+		{
+			//Move the player up
+			--posY;
+		}
+		else if(input.isKeyDown(KEY_A) || input.isKeyDown(KEY_LEFT))
+		{
+			//Move the player left
+			--posX;
+		}
+		else if(input.isKeyDown(KEY_S) || input.isKeyDown(KEY_DOWN))
+		{
+			//Move the player down
+			++posY;
+		}
+		else if(input.isKeyDown(KEY_D) || input.isKeyDown(KEY_RIGHT))
+		{
+			//Move the player right
+			++posX;
+		}
+	}
+	
+	/**
+	 * Updates the reference to the input manager.
+	 * @param input Input manager.
+	 */
+	public void setInput(Input input)
+	{
+		this.input = input;
 	}
 }
